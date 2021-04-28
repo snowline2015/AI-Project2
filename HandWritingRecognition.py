663cf7dc9b69e32c08ae3da753f36cc9a103fa06
Võ Hoàng Gia Bảo <19127102@student.hcmus.edu.vn>
2021-04-28 17:21:48 +0700

    ok

--- HandWritingRecognition.py
+++ HandWritingRecognition.py
@@ -37,17 +37,21 @@ def imshow(img, mean, std):
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
-
 dataiter = iter(train_loader)
 images, labels = dataiter.next()
 imshow(torchvision.utils.make_grid(images), 0.1307, 0.3081)
 print(labels)
 
-# Get Device
+# Device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Model
-model = Model2().to(device)
+mod_num = int(input("1. Fully Connected Layer\n2. Convolutional layer, Pooling layer, Fully connected layer\n"
+      "Choose model to train: "))
+if mod_num == 1:
+    model = Model1().to(device)
+else:
+    model = Model2().to(device)
 
 # Loss function
 criterion = nn.CrossEntropyLoss()
@@ -58,38 +62,23 @@ optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 num_steps = len(train_loader)
 
 for epoch in range(num_epochs):
-
-    # ---------- TRAINING ----------
-    # set model to training
     model.train()
-
     total_loss = 0
 
     for i, (images, labels) in enumerate(train_loader):
         images, labels = images.to(device), labels.to(device)
-
-        # Zero gradients
         optimizer.zero_grad()
-
-        # Forward
         outputs = model(images)
-
-        # Compute Loss
         loss = criterion(outputs, labels)
-
-        # Backward
         loss.backward()
         optimizer.step()
-
         total_loss += loss.item()
 
-        # Print Log
         if (i + 1) % 100 == 0:
             print("Epoch {}/{} - Step: {}/{} - Loss: {:.4f}".format(
                 epoch + 1, num_epochs, i, num_steps, total_loss / (i + 1)))
 
-    # ---------- VALIDATION ----------
-    # set model to evaluating
+    torch.save(model.state_dict(), 'model.pth')
     model.eval()
 
     val_losses = 0
@@ -99,19 +88,14 @@ for epoch in range(num_epochs):
         total = 0
         for _, (images, labels) in enumerate(val_loader):
             images, labels = images.to(device), labels.to(device)
-
             outputs = model(images)
-
             _, predicted = torch.max(outputs, 1)
-
             loss = criterion(outputs, labels)
-
             val_losses += loss.item()
-
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-        print("Epoch {} - Accuracy: {} - Validation Loss : {:.4f}".format(
+        print("Epoch {} - Accuracy: {}% - Validation Loss : {:.4f}\n".format(
             epoch + 1,
             correct / total * 100,
             val_losses / (len(val_loader))))
--- Model.py
+++ Model.py
@@ -1,18 +1,30 @@
-import numpy as np
-import torch
-import torchvision
-import matplotlib.pyplot as plt
-from torchvision import datasets, transforms
-from torch import nn, optim
+from torch import nn
 
-class Model1(nn.Module): {}
+
+class Model1(nn.Module):
+    def __init__(self, num_classes=10):
+        super(Model1, self).__init__()
+
+        self.fc1 = nn.Linear(784, 128)
+        self.fc2 = nn.Linear(128, 32)
+        self.fc3 = nn.Linear(32, num_classes)
+        self.func = nn.LeakyReLU()
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        out = self.fc1(x)
+        out = self.func(out)
+        out = self.fc2(out)
+        out = self.func(out)
+        out = self.fc3(out)
+        return out
 
 
 class Model2(nn.Module):
     def __init__(self, num_classes=10):
         super(Model2, self).__init__()
 
-        self.conv1 = nn.Sequential(
+        self.conv = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=5, padding=2, stride=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
@@ -20,8 +32,7 @@ class Model2(nn.Module):
         self.fc = nn.Linear(14 * 14 * 32, num_classes)
 
     def forward(self, x):
-        out = self.conv1(x)
+        out = self.conv(x)
         out = out.view(out.size(0), -1)
         out = self.fc(out)
-
         return out
